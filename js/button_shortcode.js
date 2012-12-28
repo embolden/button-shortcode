b6cdce788a2c66dc3c19d0b96c734d77b95f53be
matthewkeehner <matthewkeehner@gmail.com>
2012-12-28 13:36:03 -0600

    Changed tnics to tnibs.

--- js/button_shortcode.js
+++ js/button_shortcode.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function($) {
-  tinymce.create('tinymce.plugins.tnics_plugin', {
+  tinymce.create('tinymce.plugins.tnibs_plugin', {
     init : function( ed, url ) {
 
       // Register command for when button is clicked
-      ed.addCommand( 'tnics_insert_shortcode', function(u, v) {
+      ed.addCommand( 'tnibs_insert_shortcode', function(u, v) {
         selected = tinyMCE.activeEditor.selection.getContent();
 
         if( selected ) {
@@ -16,9 +16,9 @@ jQuery(document).ready(function($) {
       });
 
       // Register buttons - trigger above command when clicked
-      ed.addButton( 'tnics_button', {
+      ed.addButton( 'tnibs_button', {
         title : 'Insert shortcode',
-        cmd : 'tnics_insert_shortcode',
+        cmd : 'tnibs_insert_shortcode',
         image : url + '/path/to/image.png'
       });
 
@@ -28,5 +28,5 @@ jQuery(document).ready(function($) {
   // Register our TinyMCE plugin
   // first parameter is the button ID1
   // second parameter must match the first parameter of the tinymce.create() function above
-  tinymce.PluginManager.add( 'tnics_button', tinymce.plugins.tnics_plugin );
+  tinymce.PluginManager.add( 'tnibs_button', tinymce.plugins.tnibs_plugin );
 });
